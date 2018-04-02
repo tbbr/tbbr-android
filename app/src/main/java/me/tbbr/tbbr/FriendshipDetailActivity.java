29e36eb073c738bcfdb2bb0f5c8c653eb6f786ac
Maaz Ali <maaz_ali@outlook.com>
2018-04-02 02:26:52 -0400

    Fix the back button on the FriendshipDetailActivity

--- app/src/main/java/me/tbbr/tbbr/FriendshipDetailActivity.java
+++ app/src/main/java/me/tbbr/tbbr/FriendshipDetailActivity.java
@@ -198,18 +198,8 @@ public class FriendshipDetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
-            navigateUpTo(new Intent(this, FriendshipListActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        finish();
+        return true;
     }
 
     private void handleNullBody(Response response) {
